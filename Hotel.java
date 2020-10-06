c088848b3938b490d1eed6cdfc5e7f3cabb27ad5
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-06 17:40:14 +0530

    [Vinay]Added: Calculate rate for regular customers only

--- Hotel.java
+++ Hotel.java
@@ -1,14 +1,21 @@
 package myPackage.Vinay.HotelReservationSystem;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Hotel {
 	public String hotelName;
 	public int regularWeekdayRate;
-	public int regularWeekendRate;
-	public Hotel(String hotelName, int regularWeekdayRate, int regularWeekendRate) {
+	public int totalCostIncurred;
+	public Hotel(String hotelName, int regularWeekdayRate) {
 		super();
 		this.hotelName = hotelName;
 		this.regularWeekdayRate = regularWeekdayRate;
-		this.regularWeekendRate = regularWeekendRate;
+	}
+	public int getTotalCostIncurred() {
+		return totalCostIncurred;
 	}
 	public String getHotelName() {
 		return hotelName;
@@ -22,10 +29,35 @@ public class Hotel {
 	public void setRegularWeekdayRate(int regularWeekdayRate) {
 		this.regularWeekdayRate = regularWeekdayRate;
 	}
-	public int getRegularWeekendRate() {
-		return regularWeekendRate;
+	public void costOfStay(String entry, String exit) {
+		int totalCost =0;
+		Date entryDate = convertGivenDateToRequiredDate(entry);
+		Date exitDate = convertGivenDateToRequiredDate(exit);
+		Calendar entryDateCalendar = Calendar.getInstance();
+	    entryDateCalendar.setTime(entryDate);
+	    Calendar exitDateCalendar = Calendar.getInstance();
+	    exitDateCalendar.setTime(exitDate);
+	    for (Date date = entryDateCalendar.getTime(); entryDateCalendar.before(exitDateCalendar); entryDateCalendar.add(Calendar.DATE, 1), date = entryDateCalendar.getTime()) {
+	        int dayNumber = date.getDay();
+	        totalCost = totalCost + regularWeekdayRate;
+	    }
+	    this.totalCostIncurred = totalCost;
 	}
-	public void setRegularWeekendRate(int regularWeekendRate) {
-		this.regularWeekendRate = regularWeekendRate;
-	}		
+		
+	
+	public Date convertGivenDateToRequiredDate(String date) {
+		DateFormat df4 = new SimpleDateFormat("ddMMMyyyy");
+	    Date date1=null;		
+	    try {
+			date1 = df4.parse(date);
+			
+		} catch (Exception e) {
+			System.out.println("Invalid Date");
+		} 
+	    return date1;
+	}
+	@Override
+	public String toString() {
+		return hotelName;
+	}
 }
--- HotelReservationMain.java
+++ HotelReservationMain.java
@@ -3,13 +3,38 @@ package myPackage.Vinay.HotelReservationSystem;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import com.sun.jdi.IntegerValue;
 
 public class HotelReservationMain {
-
+	
 	public static void main(String[] args) {
+		List<Hotel> hotelList = new ArrayList<Hotel>();
 		System.out.println("Welcome to Hotel Reservation System");
-		Hotel lakeWoodHotel = new Hotel("LakeWood",110,90);
+		Hotel lakeWoodHotel = new Hotel("LakeWood",110);
+		Hotel bridgeWoodHotel = new Hotel("Bridgewood", 160);
+		Hotel ridgeWoodHotel = new Hotel("Ridgewood", 220);
+		hotelList.add(ridgeWoodHotel);
+		hotelList.add(bridgeWoodHotel);
+		hotelList.add(lakeWoodHotel);
+		for(Hotel hotel : hotelList) {
+			hotel.costOfStay("10sep2020","11sep2020");
+		}		
+		HashMap<String, Integer> costIncurredMap = (HashMap<String, Integer>) hotelList.stream().sorted(Comparator.comparing(Hotel :: getTotalCostIncurred)).collect(Collectors.toMap(Hotel :: getHotelName, Hotel :: getTotalCostIncurred ));
+		String cheapestHotelName = costIncurredMap.entrySet().stream().findFirst().get().getKey();
+		Integer cheapestRate = costIncurredMap.entrySet().stream().findFirst().get().getValue();
+		System.out.println("Cheapest Hotel is : "+cheapestHotelName +" "+cheapestRate );
+		
 	}
 }
