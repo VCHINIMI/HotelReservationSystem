9ff377cc84fc4c9acc701f5e6a325bd00bcca70e
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-06 08:52:29 +0530

    [Vinay]Added : Welcome Message

---
+++ HotelReservationMain.java
@@ -0,0 +1,9 @@
+package myPackage.Vinay.HotelReservationSystem;
+
+public class HotelReservationMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation Program");
+	}
+
+}
